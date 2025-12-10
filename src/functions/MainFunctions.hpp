658726b6db5467efc8f804a3df2a449098d252f8
nihad1213 <nihad.nemetli@gmail.com>
2025-12-11 01:08:39 +0400

    add: additions in MainFunctions.hpp file

--- src/functions/MainFunctions.hpp
+++ src/functions/MainFunctions.hpp
@@ -4,9 +4,27 @@
 #include <GLFW/glfw3.h>
 #include <string>
 #include <fstream>
+#include <vector>
 #include <iostream>
-
+#include <math.h>
 
 GLFWwindow* createWindow(int width, int height, const std::string &title);
+
+struct Vertex {
+    float x, y, z;
+};
+
+struct Face {
+    int v1, v2, v3;
+};
+
+struct Model {
+    std::vector<Vertex> vertices;
+    std::vector<Face> faces;
+};
+
+Model loadObj(const std::string &filepath);
+void renderModel(const Model &model);
+
 
 #endif
