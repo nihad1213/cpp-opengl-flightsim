dc9ff017ff9a71b2f09ae881190028563254a982
nihad1213 <nihad.nemetli@gmail.com>
2025-12-10 23:26:06 +0400

    new: simple screen created and working

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,39 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
-#include "glad/glad.h"
+// #include "glad/glad.h"
 
 int main() {
-    std::cout << "Test";
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Fligh Similator", nullptr, nullptr);
+    
+    if (!window) {
+        std::cerr << "Failed to initialize window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
 
     return 0;
 }
